c40f2ed5a68e5646fdd47154bc959c7f6d19adc5
Colin Copeland <copelco@caktusgroup.com>
2013-07-13 16:23:58 -0400

    add home page, flat ui and bootstrap

--- opendata/urls.py
+++ opendata/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from opendata.views import Home
+
 
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', Home.as_view(), name='home'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ opendata/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        return context
