40f63e8e9ce30ba26d241ea058ce499c31c883a8
Victor Rocha <victor@rochapps.com>
2013-08-06 12:55:04 -0400

    Added class Media to form to add form javascript
    to hide and show fields dynamically.

---
+++ suggestions/static/suggestions/js/form.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.control-group:has(.suggestions-hidden)').addClass("hidden");
+
+    $('#id_agency_type').bind('change', HideShowFields);
+
+    function HideShowFields(evt){
+        var options = evt.target,
+            value = options.value;
+        switch(value){
+            case "county":
+                $(".control-group:has(.suggestions-city)").addClass("hidden");
+                $(".control-group:has(.suggestions-county)").removeClass("hidden");
+                break;
+            case "city":
+                $(".control-group:has(.suggestions-city)").removeClass("hidden");
+                break;
+            default:
+                $('.control-group:has(.suggestions-hidden)').addClass("hidden");
+                break;
+        }
+    }
+});
