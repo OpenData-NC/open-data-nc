ed8bb68ef3b355de1c60fe6b14b5d190456d7e7c
Victor Rocha <victor@rochapps.com>
2013-08-08 13:09:19 -0400

    Fixed average rating for resource comments.

--- comments/models.py
+++ comments/models.py
@@ -5,5 +5,6 @@ class CommentWithRating(Comment):
     rating = models.IntegerField()
 
     def save(self, *args, **kwargs):
+        # import pdb; pdb.set_trace()
         self.content_object.rating.add(score=self.rating, user=self.user, ip_address=self.ip_address)
         super(CommentWithRating, self).save(*args, **kwargs)
