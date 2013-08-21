66890b7bd904aeb60167bd009c67026d1db683b7
Victor Rocha <victor@rochapps.com>
2013-08-21 11:18:04 -0400

    Added static pages for footer links.
    Html documents for this pages can be found inside the directory "pages".

--- opendata/urls.py
+++ opendata/urls.py
@@ -11,8 +11,28 @@ from opendata.views import Home
 admin.autodiscover()
 
 
+# Static pages
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
+    url(r'^about/$', TemplateView.as_view(
+        template_name="pages/about.html"
+        ), name='about'),
+    url(r'^faq/$', TemplateView.as_view(
+        template_name="pages/faq.html"
+        ), name='faq'),
+    url(r'^useful-links/$', TemplateView.as_view(
+        template_name="pages/useful-links.html"
+        ), name='useful-links'),
+    url(r'^privacy/$', TemplateView.as_view(
+        template_name="pages/privacy.html"
+        ), name='privacy'),
+    url(r'^terms-of-use/$', TemplateView.as_view(
+        template_name="pages/terms.html"
+        ), name='terms'),
+)
+
+# Installed apps
+urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.auth_urls')),
     url(r'^accounts/register/$', UserRegistrationView.as_view(
