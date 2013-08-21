80e1b4cb976e25e7de32c196237be2f79a1aa1e2
cmhayes <charlehm@live.unc.edu>
2013-08-21 10:17:30 -0400

    commit to update master

--- scripts/solr-rebuild-schema.sh
+++ scripts/solr-rebuild-schema.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd )"
 schema=$DIR'/apache-solr-3.6.2/website/solr/conf/schema.xml'
 python manage.py build_solr_schema --filename=$schema
-sed -i 's/words=\"stopwords_en.txt\"/words=\"lang\/stopwords_en.txt\"/' $schema
-sed -i 's/<field name=\"name\" type=\"text_en\"/<field name=\"name\" type=\"string\"/' $schema
+sed -i '' 's/words=\"stopwords_en.txt\"/words=\"lang\/stopwords_en.txt\"/' $schema
+sed -i '' 's/<field name=\"name\" type=\"text_en\"/<field name=\"name\" type=\"string\"/' $schema
