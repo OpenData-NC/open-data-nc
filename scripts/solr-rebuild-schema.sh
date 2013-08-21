f3f9f796bb460c4f2f53ccd13c37078afb339c7d
cmhayes <charlehm@live.unc.edu>
2013-08-21 11:28:21 -0400

    retry merge

--- scripts/solr-rebuild-schema.sh
+++ scripts/solr-rebuild-schema.sh
@@ -2,5 +2,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd )"
 schema=$DIR'/apache-solr-3.6.2/website/solr/conf/schema.xml'
 python manage.py build_solr_schema --filename=$schema
-sed -i '' 's/words=\"stopwords_en.txt\"/words=\"lang\/stopwords_en.txt\"/' $schema
-sed -i '' 's/<field name=\"name\" type=\"text_en\"/<field name=\"name\" type=\"string\"/' $schema
+if [ "$(uname)" == "Darwin" ]; then
+    # Mac OS X has a different implementation for sed.
+    # http://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-and-linux
+    sed -i '' 's/words=\"stopwords_en.txt\"/words=\"lang\/stopwords_en.txt\"/' $schema
+    sed -i '' 's/<field name=\"name\" type=\"text_en\"/<field name=\"name\" type=\"string\"/' $schema
+else
+    sed -i 's/words=\"stopwords_en.txt\"/words=\"lang\/stopwords_en.txt\"/' $schema
+    sed -i 's/<field name=\"name\" type=\"text_en\"/<field name=\"name\" type=\"string\"/' $schema
+fi
