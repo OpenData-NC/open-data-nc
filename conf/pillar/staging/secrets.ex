c1e250fe41c5f76b0abe0e9693f86dbb41a9fb3b
Victor Rocha <victor@rochapps.com>
2013-11-19 11:25:10 -0500

    [fixes #124] Added new keys to secrets.ex

--- conf/pillar/staging/secrets.ex
+++ conf/pillar/staging/secrets.ex
@@ -3,6 +3,9 @@ secrets:
   DB_PASSWORD: XXXXXX
   SECRET_KEY: XXXXXX
   BROKER_PASSWORD: XXXXXX
+  EMAIL_USER: XXXXXX
+  EMAIL_PASSWORD: XXXXXX
+  RECAPTCHA_PRIVATE_KEY = XXXXXX
 
 # Uncomment and update username/password to enable HTTP basic auth
 # http_auth:
