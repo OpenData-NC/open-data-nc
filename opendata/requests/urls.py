d4210bfb2687de791f9d1d623873ce952f6c14a2
Victor Rocha <victor@rochapps.com>
2013-11-22 11:33:43 -0500

    [refs #105] New request detail page.
    - Links to request detail page from request list page.
    - Vote up and Down functionality.

--- opendata/requests/urls.py
+++ opendata/requests/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, url
-from opendata.requests import views
 
+from . import views
+from .views import RequestDetailView
 
 urlpatterns = patterns('',
    url(r'^$', views.list_requests, name="request-list"),
    url(r'^create/$', views.add_request, name="request-create"),
-   url(r'^(?P<request_id>\d+)/vote/$', views.vote,
-       name="request-vote"),
-   url(r'^(?P<request_id>\d+)/unvote/$', views.remove_vote,
-       name="request-remove-vote"),
+   url(r'^(?P<pk>\d+)/$', RequestDetailView.as_view(), name="requests_request_detail"),
+   url(r'^(?P<request_id>\d+)/vote/$', views.vote, name="request-vote"),
+   url(r'^(?P<request_id>\d+)/unvote/$', views.remove_vote, name="request-remove-vote"),
 )
--- opendata/requests/views.py
+++ opendata/requests/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
+from django.views.generic import DetailView
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
+
 from djangoratings.exceptions import CannotDeleteVote
 
 from .models import *
@@ -23,6 +25,22 @@ def list_requests(request):
         'requests': requests,
     }
     return render(request, 'requests/list.html', context)
+
+
+class RequestDetailView(DetailView):
+    model = Request
+
+    def get_queryset(self):
+        qs = super(RequestDetailView, self).get_queryset()
+        return qs.filter(status=self.model.APPROVED)
+
+    def get_context_data(self, **kwargs):
+        context = super(RequestDetailView, self).get_context_data(**kwargs)
+        request = context['object']
+        votes = request.rating.votes
+        site_url = getattr(settings, 'SITE_URL', 'http://localhost')
+        context.update({'site_url': site_url, 'votes': votes})
+        return context
 
 
 @login_required
@@ -49,13 +67,14 @@ def add_request(request):
 @require_POST
 def vote(request, request_id):
     """Vote for a requests"""
+    redirect_url = request.POST.get('next', reverse('request-list'))
     request_object = get_object_or_404(Request, pk=request_id)
     voted = request_object.rating.get_rating_for_user(request.user,
                                               request.META['REMOTE_ADDR'])
     if not voted:
         request_object.rating.add(score=1, user=request.user,
                            ip_address=request.META['REMOTE_ADDR'])
-    return redirect(reverse('request-list'))
+    return redirect(redirect_url)
 
 
 @login_required
@@ -63,9 +82,10 @@ def vote(request, request_id):
 def remove_vote(request, request_id):
     """Remove pre-existing vote for requests"""
     request_object = get_object_or_404(Request, pk=request_id)
+    redirect_url = request.POST.get('next', reverse('request-list'))
     try:
         request_object.rating.delete(request.user, request.META['REMOTE_ADDR'])
     except CannotDeleteVote:
         # vote didn't exist, just move on
         pass
-    return redirect(reverse('request-list'))
+    return redirect(redirect_url)
